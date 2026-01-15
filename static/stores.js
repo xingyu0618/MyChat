a821ce971fbf7f9e1c5ff92602e65ec162af8f43
xylin42 <120558022+xylin42@users.noreply.github.com>
2026-01-15 19:54:41 +0800

    Add IndexedDB reads for chat and friend stores

--- static/stores.js
+++ static/stores.js
@@ -1,18 +1,26 @@
 import {RecentChatList} from "./recentChatList";
-import {deleteDB, putChatsToDB, putFriendsToDB} from "./db";
+import {deleteDB, getChatsFromDB, getFriendsFromDB, putChatsToDB, putFriendsToDB} from "./db";
 import Alpine from "alpinejs";
+
+function getJsonScriptData(id) {
+    const element = document.getElementById(id)
+    if (!element) {
+        return null
+    }
+    return JSON.parse(element.textContent)
+}
 
 async function loadFull() {
     await deleteDB()
 
-    const friends = JSON.parse(document.getElementById('friends').textContent)
-    const states = JSON.parse(document.getElementById('states').textContent)
+    const friends = getJsonScriptData('friends') ?? []
+    const states = getJsonScriptData('states') ?? []
 
     friendStore.setFriends(friends)
     chatStore.rebuildFrom(states)
 
-    putFriendsToDB(friends)
-    putChatsToDB(states)
+    await putFriendsToDB(friends)
+    await putChatsToDB(states)
 }
 
 function setupChatStore() {
@@ -27,6 +35,20 @@ function setupChatStore() {
             this.refresh()
         },
         loadFull,
+        async loadFromDB() {
+            const chats = await getChatsFromDB()
+            if (chats.length > 0) {
+                this.rebuildFrom(chats)
+            }
+        },
+        async loadPartial() {
+            await this.loadFromDB()
+            const partialStates = getJsonScriptData('partial_states') ?? []
+            if (partialStates.length > 0) {
+                this.rebuildFrom(partialStates)
+                await putChatsToDB(partialStates)
+            }
+        },
         put(conv) {
             this.manager.upsertToFront(conv)
             this.refresh()
@@ -40,23 +62,40 @@ function setupChatStore() {
             this.refresh()
         }
     })
-    return Alpine.store(chatStoreName)
+    const store = Alpine.store(chatStoreName)
+    store.init()
+    return store
 }
 
 function setupFriendStore(alpine) {
     const friendStoreName = 'friends'
     Alpine.store(friendStoreName, {
-        friends: {},
+        friends: [],
+        friendMap: {},
+        init() {
+            this.loadFromDB()
+        },
+        async loadFromDB() {
+            const friends = await getFriendsFromDB()
+            if (friends.length > 0) {
+                this.setFriends(friends)
+            }
+        },
         setFriends(data) {
+            this.friends = []
+            this.friendMap = {}
             for (const item of data) {
-                this.friends[item.id] = item
+                this.friends.push(item)
+                this.friendMap[item.id] = item
             }
         },
         getFriend(id) {
-            return this.friends[id]
+            return this.friendMap[id]
         }
     })
-    return Alpine.store(friendStoreName)
+    const store = Alpine.store(friendStoreName)
+    store.init()
+    return store
 }
 
 export const chatStore = setupChatStore()
